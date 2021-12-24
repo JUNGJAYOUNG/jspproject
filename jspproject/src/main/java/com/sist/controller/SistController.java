9604f84cda6e26e684dcba1546e070527872fcab
yaps1 <hyunho0712@naver.com>
2021-12-24 20:07:15 +0900

    sistController.java hashmap추가

--- jspproject/src/main/java/com/sist/controller/SistController.java
+++ jspproject/src/main/java/com/sist/controller/SistController.java
@@ -1,7 +1,12 @@
 package com.sist.controller;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Properties;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,13 +14,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sist.action.SistAction;
+
 /**
  * Servlet implementation class SistController
  */
 @WebServlet("*.do")
 public class SistController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	HashMap<String, SistAction> map;
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -28,7 +35,22 @@ public class SistController extends HttpServlet {
     public void init(ServletConfig config) throws ServletException {
     	// TODO Auto-generated method stub
     	//super.init(config);
-    	
+		map=new HashMap<String, SistAction>();
+		try {
+			String path = config.getServletContext().getRealPath("WEB-INF");
+			FileReader fr = new FileReader(path+"/sist.properties");
+			Properties prop = new Properties();
+			prop.load(fr);
+			Iterator keyList=prop.keySet().iterator();
+			while(keyList.hasNext()) {
+				String key = (String)keyList.next();
+				String clsName=prop.getProperty(key);
+				SistAction obj=(SistAction)Class.forName(clsName).newInstance();
+				map.put(key, obj);
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());// TODO: handle exception
+		}
     }
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -48,7 +70,14 @@ public class SistController extends HttpServlet {
 		proRequest(request, response);
 	}
 	public void proRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri= request.getRequestURI();
 		
+		String cmd = uri.substring(uri.lastIndexOf("/")+1);
+		SistAction action=map.get(cmd);
+		System.out.println(cmd);
+		String viewPage=action.proRequest(request, response);
+		RequestDispatcher dispatcher=request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
 	}
 
 }
